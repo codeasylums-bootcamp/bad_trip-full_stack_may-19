562ab2cff5bebe0d2a61801087af3b89b4b4fa20
Priyadarshan Vijay <Priyadarshanvijay@gmail.com>
2019-06-05 15:25:06 +0530

    key authentication using POST method

--- login.js
+++ login.js
@@ -8,12 +8,34 @@ router.get('/',function(req,res){
     res.send("users home").status(200);
 });
 
-router.post('/',function(req,res){
+
+
+
+router.post('/existingUserLogin',function(req,res){
+    var username  = req.body.name;
+    var password = req.body.password;
+
+    loginmodel.findOne({name: username, password: password}, function(err,user){
+        if(err){
+            console.log(err);
+            return res.status(500).send();
+        }
+
+        if(!user){
+            return res.status(404).send();
+        }
+
+        return res.status(200).send();
+    })
+});
+
+router.post('/register',function(req,res){
    // console.log(req.body);
     //res.json(req.body).status(200);
     const newlogin = new loginmodel({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
+        email: req.body.email,
         password: req.body.password
     });
     loginmodel.find({name:req.body.name})
--- loginmodel.js
+++ loginmodel.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const loginSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: {type: String, required:true},
-    //email:{type: String, required: true, unique:true},
+    email:{type: String, required: true, unique:true},
     password:{type: String, required: true},
-
-
+    curLevel:{type:Number, default: 1},
 });
 module.exports = mongoose.model('login',loginSchema);

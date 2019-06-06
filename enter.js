6e894a0171096194c6079088f898d5f890b954d9
Priyadarshan Vijay <Priyadarshanvijay@gmail.com>
2019-06-06 12:39:43 +0530

    Closing tags fix in page 2 and 4

--- enter.js
+++ enter.js
@@ -33,15 +33,33 @@ app.use('*',function(req,res,next){
     next();
 });
 
-app.get('/*', function(req,res) {
+app.get('/*', function(req,res,next) {
     data= fs.readFile('./' + req.url,   function (err, data) {
     res.setHeader('Content-Type', 'text/html');
     res.send(data);});
+    next();
 });
 
 //app.use('/reg',rege);
 app.use('/login',login);
 app.use('/clearLevel',clearLevel);
+// Access the leaderboard
+app.get('/players', async function(req, res) {
+    // retrieve ‘lim’ from the query string info
+    let { lim } = req.query;
+    db.collection('players')
+        .find()
+        // -1 is for descending and 1 is for ascending
+        .sort({ curLevel: -1 })
+        // Show only [lim] players
+        .limit(lim)
+        .toArray(function(err, result) {
+            if (err)
+                res.send({ status: false, msg: 'failed to retrieve players' });
+            console.log(Array.from(result));
+            res.send({ status: true, msg: result });
+        });
+ });
 //app.use('/order',orders)
 
 
